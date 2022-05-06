2a6c4b5979f3da6a5d5c7b015603ff2759617c2c
SapitoBot <103408838+AgromosSpboy@users.noreply.github.com>
2022-05-06 19:08:16 -0400

    Update boost.js

--- plugins/boost.js
+++ plugins/boost.js
@@ -18,7 +18,7 @@ let handler = async (m, { conn }) => {
    let old = performance.now()
    let neww = performance.now()
    let speed = `${neww - old}`
-   let finish = `*_⚡ Bot Agromos SP acelerado con éxito_*\n\n*_Acelerado: ${speed} segundos!_*`
+   let finish = `*_⚡ Bot Sapito acelerado con éxito_*\n\n*_Acelerado: ${speed} segundos!_*`
 
      conn.reply(m.chat, finish, m)
 }
